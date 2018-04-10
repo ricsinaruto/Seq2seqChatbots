d9fde8c23de738f5e0fa64ac7e171a6b12d02487
Csáky Richárd Krisztián <ricsinaruto@hotmail.com>
2018-04-10 17:21:09 +0200

    minor fix

--- t2t_csaky/__init__.py
+++ t2t_csaky/__init__.py
@@ -8,6 +8,7 @@ This file is the main python file of the project importing all my
 from t2t_csaky.problems import character_chatbot
 from t2t_csaky.problems import cornell_chatbots
 from t2t_csaky.problems import daily_dialog_chatbot
+from t2t_csaky.problems import persona_chat_chatbot
 
 from t2t_csaky.models import roulette_transformer
 from t2t_csaky.models import gradient_checkpointed_seq2seq
